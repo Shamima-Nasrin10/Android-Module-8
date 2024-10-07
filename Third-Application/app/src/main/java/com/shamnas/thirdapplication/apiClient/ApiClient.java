47c5f094285106bb689e54e960e4b9a932d13d5e
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-10-07 18:53:10 +0600

    nothing

---
+++ Third-Application/app/src/main/java/com/shamnas/thirdapplication/apiClient/ApiClient.java
@@ -0,0 +1,13 @@
+package com.shamnas.thirdapplication.apiClient;
+
+import retrofit2.Retrofit;
+
+public class ApiClient {
+
+//    public  static final BASE_URL="http://purbachal.emranhss.com/";
+
+    private static Retrofit retrofit;
+
+
+
+}
